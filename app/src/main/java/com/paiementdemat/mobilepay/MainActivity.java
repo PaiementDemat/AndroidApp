5369d6e5f0d74b8e087cfed5f3484ac039109172
shenzou <alexandre.shenouda@devinci.fr>
2019-11-19 01:57:40 +0100

    Working QR code generation, Working NFC Ndef Tag detection and reading.

--- app/src/main/java/com/paiementdemat/mobilepay/MainActivity.java
+++ app/src/main/java/com/paiementdemat/mobilepay/MainActivity.java
@@ -1,16 +1,22 @@
 package com.paiementdemat.mobilepay;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +24,38 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle t;
     private NavigationView nv;
     private Intent intent;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.System.canWrite(getApplicationContext())) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+                dialog.setMessage(R.string.AskAuthorizations);
+                dialog.setTitle(R.string.Authorization);
+                dialog.setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName()));
+                        startActivityForResult(intent, 200);
+                    }
+                });
+                dialog.setNegativeButton(R.string.No, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), R.string.AuthorizationsRemember, Toast.LENGTH_LONG).show();
+                    }
+                });
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+
+            }
+        }
+
+        textView = findViewById(R.id.textView);
 
         dl = findViewById(R.id.activity_main);
         t = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
@@ -54,22 +86,18 @@ public class MainActivity extends AppCompatActivity {
                     default:
                         return true;
                 }
-
-
                 return true;
 
             }
         });
-
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         if (t.onOptionsItemSelected(item))
             return true;
-
         return super.onOptionsItemSelected(item);
     }
+
+
 }
--- app/src/main/java/com/paiementdemat/mobilepay/QRGen.java
+++ app/src/main/java/com/paiementdemat/mobilepay/QRGen.java
@@ -2,14 +2,72 @@ package com.paiementdemat.mobilepay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 public class QRGen extends AppCompatActivity {
+
+    private Button generate;
+    private ImageView imageView;
+    private EditText editText;
+    private TextView infoDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrgen);
+        generate = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+        editText = findViewById(R.id.price);
+        infoDisplay = findViewById(R.id.infoDisplay);
+
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                QRCodeWriter qrCodeWriter = new QRCodeWriter();
+                try{
+                    BitMatrix bitMatrix = qrCodeWriter.encode(editText.getText().toString(), BarcodeFormat.QR_CODE, 2000, 2000);
+                    //infoDisplay.setText(editText.getText().toString());
+                    int height = bitMatrix.getHeight();
+                    int width = bitMatrix.getWidth();
+                    Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+                    for(int x=0; x<width; x++){
+                        for(int y=0; y<height; y++){
+                            bmp.setPixel(x, y, bitMatrix.get(x,y) ? Color.BLACK : Color.WHITE);
+                        }
+                    }
+                    imageView.setImageBitmap(bmp);
+                    Context context = getApplicationContext();
+                    Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 50);
+                    Window window = getWindow();
+                    //Get the current window attributes
+                    WindowManager.LayoutParams layoutpars = window.getAttributes();
+                    //Set the brightness of this window
+                    layoutpars.screenBrightness = 100 / (float)255;
+                    //Apply attribute changes to this window
+                    window.setAttributes(layoutpars);
+                }
+                catch(Exception e){
+                    Toast toast = Toast.makeText(getApplicationContext(), R.string.ErrorQRGen, Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
     }
 
 }
